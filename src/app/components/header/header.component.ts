b2a07cb8e60b199c52bd02510aa8309de33fa500
devansheee <devanshee.vankani@jeavio.com>
2022-01-31 15:11:15 +0530

    add task form done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,11 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
-  constructor() {}
+  showAddTask: boolean = false;
+  subsciption: Subscription = new Subscription();
+
+  constructor(private uiService: UiService) {
+    this.subsciption = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {}
 
   toggleAddTask() {
     console.log('toggle');
+    this.uiService.toggleAddTask();
   }
 }
